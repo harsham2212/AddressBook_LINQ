6e1900b94d3ef86a0449b1f5eb55535d04288176
Harsh Singh <kmrharsh50@gmail.com>
2022-01-24 00:25:17 +0530

    [Harsh]Added:Welcome Message Addressbook_LINQ feature implemented

---
+++ AddressBook_LINQ/AddressBook_LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook LINQ problem Statement");
+        }
+    }
+}
